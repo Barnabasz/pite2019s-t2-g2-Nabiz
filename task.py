fa6fe8d2981a4a608280e6bb4869094584fcaa26
Dawid Gwizdz <6gwizdzd@taurus.fis.agh.edu.pl>
2019-03-29 14:30:55 +0100

    task2

--- task.py
+++ task.py
@@ -1,26 +1,48 @@
-#
-#Banking simulator. Write a code in python that simulates the banking system. 
-#The program should:
-# - be able to create new banks
-# - store client information in banks
-# - allow for cash input and withdrawal
-# - allow for money transfer from client to client
-#If you can think of any other features, you can add them.
-#This code shoud be runnable with 'python kol1.py'.
-#You don't need to use user input, just show me in the script that the structure of your code works.
-#If you have spare time you can implement: Command Line Interface, some kind of data storage, or even multiprocessing.
-#
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#Your program must be runnable with command "python task.py".
-#Show some usecases of your library in the code (print some things)
-#
-#When you are done upload this code to your github repository. 
-#
-#Delete these comments before commit!
-#Good luck.
+#!/usr/bin/python3
+
+class Bank():
+	def __init__(self,name):
+		self.name = name
+		self.accounts = {}
+	def addAccount(self,client):
+		acc = Account(client)
+		self.accounts[acc.number] = acc
+	def event(self,accNum, evName, money=0):
+		if evName == "deposit":
+			self.accounts[accNum].deposit(money)
+		elif evName == "withdraw":
+			self.accounts[accNum].withdraw(money)
+		elif evName == "balance":
+			return self.accounts[accNum].getBalance()
+
+class Client():
+	def __init__(self,name):
+		self.name = name
+	
+class Account():
+	number = 0
+	def __init__(self, client):
+		Account.number +=1
+		self.number = Account.number
+		self.balance = 0
+		self.client = client
+	def getBalance(self):
+		return self.balance
+
+	def deposit(self,money):
+		self.balance+=money
+
+	def withdraw(self,money):
+		self.balance-=money
+
+		
+client1 = Client("David")
+print(client1.name)
+
+bank1 = Bank("PKO")
+bank1.addAccount(client1)
+
+bank1.event(1,"deposit",100)
+print(bank1.event(1,"balance"))
+bank1.event(1,"withdraw",50)
+print(bank1.event(1,"balance"))
